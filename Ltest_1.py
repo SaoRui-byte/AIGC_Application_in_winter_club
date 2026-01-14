7f9597918f59f044e6710b8a3cfcce5937244b0b
saorui <1639035127@qq.com>
2026-01-14 10:09:04 +0800

    简单的一个对话测试代码

---
+++ Ltest_1.py
@@ -0,0 +1,35 @@
+from zhipuai import ZhipuAI
+import time
+client = ZhipuAI(api_key="保密")
+
+
+def stream_ouput(stream_data):
+    for i in stream_data:
+        print(i,end='')
+        time.sleep(0.05)
+        
+def chat(message):
+    response = client.chat.completions.create(
+        model="glm-4",
+        messages=[
+            {"role": "system", "content": f"你是钢铁侠里的Javis,我是你的boss"},
+            {"role": "user", "content": message},
+        ],
+    )
+    return response.choices[0].message.content
+
+stream_ouput("请输入你的专属AI名称：")
+n = input()
+
+
+while True:
+    stream_ouput(f'我是{n}，有什么可以帮到你的：')
+    message = input()
+    if(message == '再见'):
+        print('bye')
+        break
+
+    stream_ouput(chat(message))
+    print('\n')
+
+
